144189eacc2b0315a60451ecb252b82f9a9e1fce
Martin Koeller <koeller2203@gmail.com>
2023-01-30 10:53:48 +0100

    update frontend

--- website/views.py
+++ website/views.py
@@ -9,3 +9,9 @@ views = Blueprint('views', __name__)
 def home():
 
     return render_template("home.html", user=current_user)
+
+
+@views.route('/manage/', methods=['GET', 'POST'])
+def manage():
+
+    return render_template("manage.html", user=current_user)
